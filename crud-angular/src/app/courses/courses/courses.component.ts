b17502b2edf53cca767933c6d7051cdd6705ff53
vinicius <viniciusarashiro0@gmail.com>
2023-05-29 16:55:51 -0300

    feat: handling errors with MatDialog

--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -1,7 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {Course} from "../model/course";
 import {CoursesService} from "../services/courses.service";
-import {Observable} from "rxjs";
+import {catchError, Observable, of} from "rxjs";
+import {MatDialog} from "@angular/material/dialog";
+import {ErrorDialogComponent} from "../../shared/components/error-dialog/error-dialog.component";
 
 @Component({
   selector: 'app-courses',
@@ -16,8 +18,23 @@ export class CoursesComponent implements OnInit{
 
   // coursesService: CoursesService;
 
-  constructor(private coursesService: CoursesService) {
-    this.courses$ = this.coursesService.list();
+  constructor(
+    private coursesService: CoursesService,
+    public dialog: MatDialog
+  ) {
+    this.courses$ = this.coursesService.list()
+      .pipe(
+        catchError(error => {
+          this.onError('Erro ao carregar cursos.')
+          return of([])
+        })
+      );
+  }
+
+  onError(errorMsg: string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: errorMsg,
+    });
   }
 
   ngOnInit() {
