db0773b4bcb5c90218eecf884895cf2ec8489969
vinicius <viniciusarashiro0@gmail.com>
2023-05-29 16:03:42 -0300

    feat: create courses service

--- crud-angular/src/app/app.module.ts
+++ crud-angular/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HttpClientModule} from "@angular/common/http";
 
 
 @NgModule({
@@ -15,7 +16,8 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Course} from "../model/course";
+import {CoursesService} from "../services/courses.service";
 
 @Component({
   selector: 'app-courses',
@@ -8,12 +9,14 @@ import {Course} from "../model/course";
 })
 export class CoursesComponent implements OnInit{
 
-  courses: Course[] = [
-    {_id: '1', name: 'Angular', category: 'front-end'}
-  ]
+  courses: Course[] = []
+
   displayedColumns = ['name', 'category']
 
-  constructor() {
+  // coursesService: CoursesService;
+
+  constructor(private coursesService: CoursesService) {
+    this.courses = this.coursesService.list();
   }
 
   ngOnInit() {
