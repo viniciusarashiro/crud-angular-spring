481cc449604fb32fe034b5851f50d79ef5fabcae
vinicius <viniciusarashiro0@gmail.com>
2023-05-30 14:43:17 -0300

    feat: get all courses

---
+++ crud-spring/src/main/java/com/vinicius/controller/CourseController.java
@@ -0,0 +1,23 @@
+package com.vinicius.controller;
+
+import com.vinicius.model.Course;
+import com.vinicius.repository.CourseRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/courses")
+@AllArgsConstructor
+public class CourseController {
+
+    private final CourseRepository courseRepository;
+
+    @GetMapping
+    public List<Course> list() {
+        return courseRepository.findAll();
+    }
+}
---
+++ crud-spring/src/main/java/com/vinicius/repository/CourseRepository.java
@@ -0,0 +1,10 @@
+package com.vinicius.repository;
+
+import com.vinicius.model.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CourseRepository extends JpaRepository<Course, Long> {
+
+}
