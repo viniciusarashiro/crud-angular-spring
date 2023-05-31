9bc1152053786a4f70c3673bbb6b34aefb4af41f
vinicius <viniciusarashiro0@gmail.com>
2023-05-31 14:59:14 -0300

    feat: component form

--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -4,6 +4,7 @@ import {CoursesService} from "../services/courses.service";
 import {catchError, Observable, of} from "rxjs";
 import {MatDialog} from "@angular/material/dialog";
 import {ErrorDialogComponent} from "../../shared/components/error-dialog/error-dialog.component";
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-courses',
@@ -14,13 +15,15 @@ export class CoursesComponent implements OnInit{
 
   courses$: Observable<Course[]>
 
-  displayedColumns = ['name', 'category']
+  displayedColumns = ['name', 'category', 'actions']
 
   // coursesService: CoursesService;
 
   constructor(
     private coursesService: CoursesService,
-    public dialog: MatDialog
+    public dialog: MatDialog,
+    private router: Router,
+    private route: ActivatedRoute
   ) {
     this.courses$ = this.coursesService.list()
       .pipe(
@@ -38,6 +41,10 @@ export class CoursesComponent implements OnInit{
   }
 
   ngOnInit() {
+  }
+
+  onAdd() {
+    this.router.navigate(['new'], {relativeTo: this.route})
   }
 
 }
