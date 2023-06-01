228bbf04ec0baa6181e5254d1006244b78fe987a
vinicius <viniciusarashiro0@gmail.com>
2023-06-01 15:55:29 -0300

    refactor: components and containers

--- crud-angular/src/app/courses/courses.module.ts
+++ crud-angular/src/app/courses/courses.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CoursesRoutingModule } from './courses-routing.module';
-import { CoursesComponent } from './courses/courses.component';
+import { CoursesComponent } from './containers/courses/courses.component';
 import {AppMaterialModule} from "../shared/app-material/app-material.module";
 import {SharedModule} from "../shared/shared.module";
-import { CourseFormComponent } from './course-form/course-form.component';
+import { CourseFormComponent } from './containers/course-form/course-form.component';
 import {ReactiveFormsModule} from "@angular/forms";
-import { CoursesListComponent } from './courses-list/courses-list.component';
+import { CoursesListComponent } from './components/courses-list/courses-list.component';
 
 
 @NgModule({
