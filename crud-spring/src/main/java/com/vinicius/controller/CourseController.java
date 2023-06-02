e118a43c495d6074ddeefd41160d1aefd09a3e9a
vinicius <viniciusarashiro0@gmail.com>
2023-06-02 16:43:21 -0300

    feat: validation back-end

--- crud-spring/src/main/java/com/vinicius/controller/CourseController.java
+++ crud-spring/src/main/java/com/vinicius/controller/CourseController.java
@@ -2,15 +2,20 @@ package com.vinicius.controller;
 
 import com.vinicius.model.Course;
 import com.vinicius.repository.CourseRepository;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Validated
 @RestController
 @RequestMapping("/api/courses")
 @AllArgsConstructor
@@ -24,7 +29,7 @@ public class CourseController {
     }
     
     @GetMapping("/{id}")
-    public ResponseEntity<Course> findById(@PathVariable Long id) {
+    public ResponseEntity<Course> findById(@PathVariable  @NotNull @Positive Long id) {
         return courseRepository.findById(id)
                 .map(recordFound -> ResponseEntity.ok().body(recordFound))
                 .orElse(ResponseEntity.notFound().build());
@@ -32,12 +37,12 @@ public class CourseController {
 
     @PostMapping
     @ResponseStatus(code = HttpStatus.CREATED)
-    public Course create(@RequestBody Course course) {
+    public Course create(@RequestBody @Valid Course course) {
         return courseRepository.save(course);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Course>  update(@PathVariable Long id, @RequestBody Course course) {
+    public ResponseEntity<Course>  update(@PathVariable  @NotNull @Positive Long id, @RequestBody @Valid Course course) {
         return courseRepository.findById(id)
                 .map(recordFound -> {
                     recordFound.setName(course.getName());
@@ -49,7 +54,7 @@ public class CourseController {
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> delete(@PathVariable Long id) {
+    public ResponseEntity<Void> delete(@PathVariable @NotNull @Positive Long id) {
         return courseRepository.findById(id)
                 .map(recordFound -> {
                     courseRepository.deleteById(id);
--- crud-spring/src/main/java/com/vinicius/model/Course.java
+++ crud-spring/src/main/java/com/vinicius/model/Course.java
@@ -1,8 +1,13 @@
 package com.vinicius.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.annotation.Nullable;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
+import org.antlr.v4.runtime.misc.NotNull;
+import org.hibernate.validator.constraints.Length;
 
 
 @Data
@@ -14,9 +19,15 @@ public class Course {
     @JsonProperty("_id")
     private Long id;
 
-    @Column(length = 200, nullable = false)
+    @NotBlank
+    @NotNull
+    @Length(min = 5, max = 100)
+    @Column(length = 100, nullable = false)
     private String name;
 
+    @NotBlank
+    @Length(max = 10)
+    @Pattern(regexp = "Back-end|Front-end")
     @Column(length = 10, nullable = false)
     private String category;
 }
