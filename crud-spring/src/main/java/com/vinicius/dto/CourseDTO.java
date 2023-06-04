ce7f435b5a4e7a1b1a9a3cc47f70b8e603a91caf
vinicius <viniciusarashiro0@gmail.com>
2023-06-04 16:17:09 -0300

    feat: list lessons

--- crud-spring/src/main/java/com/vinicius/dto/CourseDTO.java
+++ crud-spring/src/main/java/com/vinicius/dto/CourseDTO.java
@@ -1,13 +1,17 @@
 package com.vinicius.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.vinicius.model.Lesson;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import org.antlr.v4.runtime.misc.NotNull;
 import org.hibernate.validator.constraints.Length;
 
+import java.util.List;
+
 public record CourseDTO(
         @JsonProperty("_id") Long id,
         @NotBlank @NotNull @Length(min = 5, max = 100) String name,
-        @NotNull @Length(max = 10) @Pattern(regexp = "Back-end|Front-end") String category) {
+        @NotNull @Length(max = 10) @Pattern(regexp = "Back-end|Front-end") String category,
+        List<Lesson> lessons) {
 }
--- crud-spring/src/main/java/com/vinicius/model/Lesson.java
+++ crud-spring/src/main/java/com/vinicius/model/Lesson.java
@@ -1,5 +1,6 @@
 package com.vinicius.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.Data;
 
@@ -18,5 +19,6 @@ public class Lesson {
 
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "course_id", nullable = false)
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private Course course;
 }
